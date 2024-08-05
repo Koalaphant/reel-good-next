6fec64c1bfc41731f46ecc28c803179100b4a2e4
Koalaphant <andrew.wardjones@icloud.com>
2024-08-05 17:24:03 +0100

    created about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,32 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie discovery platform, where film enthusiasts can dive
+        deep into the world of cinema. Our site offers a comprehensive database
+        of movies, providing detailed information such as plot summaries, cast
+        and crew details, ratings, and more. Whether you are searching for the
+        latest blockbusters or classic films, you will find all the information
+        you need to satisfy your movie cravings.
+      </p>
+
+      <p>
+        Explore a vast collection of movies and discover hidden gems with our
+        intuitive search and filter features. Our user-friendly interface allows
+        you to browse through different genres, release years, and popularity
+        rankings, making it easy to find exactly what you are looking for. Plus,
+        get personalized recommendations based on your viewing history and
+        preferences to enhance your movie-watching experience.
+      </p>
+
+      <p>
+        Stay up-to-date with the latest film news and reviews, and engage with
+        our community of movie lovers. Share your thoughts, read reviews from
+        fellow enthusiasts, and participate in discussions about your favorite
+        films. With our platform, you can stay connected to the world of cinema
+        and enjoy a rich, interactive movie experience like never before.
+      </p>
+    </div>
+  );
+}
