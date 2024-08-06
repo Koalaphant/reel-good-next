3c85206be18d8f50121cb8a95a9fc8946c9bae52
Koalaphant <andrew.wardjones@icloud.com>
2024-08-06 17:48:37 +0100

    added search functionality

---
+++ src/app/search/[searchTerm]/page.tsx
@@ -0,0 +1,28 @@
+import Results from "@/components/Results";
+
+interface SearchPageProps {
+  params: {
+    searchTerm: string; // Type the searchTerm as a string; adjust if it is different
+  };
+}
+
+export default async function SearchPage({ params }: SearchPageProps) {
+  const { searchTerm } = params;
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
+  );
+
+  const data = await res.json();
+  const results = data.results;
+
+  return (
+    <div>
+      {results && results.length === 0 && (
+        <h1 className="text-center pt-6">No results found</h1>
+      )}
+
+      {results && <Results results={results} />}
+    </div>
+  );
+}
